bc0aac1be1883b2ae8b1102e03f816a8f9e57901
weihsin <hsinwe2016@gmail.com>
2017-08-16 00:12:51 +0800

    add management command against staging server for creating sessions

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,13 +1,47 @@
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-import re
+import re, os, poplib, time
 
 from .base import FunctionalTest
 
-TEST_EMAIL = 'edith@example'
+TEST_EMAIL = '492407290@qq.com'
 SUBJECT = 'Your login link for Superlists'
 
 class LoginTest(FunctionalTest):
+    
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.qq.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['QQ_PASSWORD'])
+            while time.time() - start < 60:
+                # get 10 newest messages
+                count, _ = inbox.stat()
+                print('count: ', count)
+                for i in reversed(range(max(1, count -10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf8') for l in lines]
+                    print(len(lines))
+                    for l in lines:
+                        if f'Subject: {subject}' in l:
+                            email_id = i
+                            body = '\n'.join(lines)
+                            print('body: ', type(body))
+                            return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
 
     def test_can_get_email_link_to_log_in(self):
         # Edith goes to the awesome superlists
@@ -24,15 +58,13 @@ class LoginTest(FunctionalTest):
         ))
 
         # She checks her email and finds a message
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(TEST_EMAIL, SUBJECT)
 
         # It has a url link in it
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'http://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
+            self.fail(f'Could not find url in email body:\n{body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
